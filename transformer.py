689e9dc8f8ebc9669fc4023b178a71bb807c7864
Ben Kwan <benkwan26@gmail.com>
2026-02-10 14:25:49 -0500

    Finish transformer components

--- transformer.py
+++ transformer.py
@@ -182,3 +182,97 @@ class Encoder(nn.Module):
         x = self.layers(x, self_attention_mask)
 
         return x
+    
+class MultiHeadCrossAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.kv_layer = nn.Linear(d_model, 2 * d_model)
+        self.q_layer = nn.Linear(d_model, d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+    
+    def forward(self, x, y, mask):
+        batch_size, sequence_length, d_model = x.size()
+        kv = self.kv_layer(x)
+        q = self.q_layer(y)
+        kv = kv.reshape(batch_size, sequence_length, self.num_heads, 2 * self.head_dim)
+        q = q.reshape(batch_size, sequence_length, self.num_heads, self.head_dim)
+        kv = kv.permute(0, 2, 1, 3)
+        q = q.permute(0, 2, 1, 3)
+        k, v = kv.chunk(2, dim=-1)
+        values, _ = scaled_dot_product(q, k, v, mask)
+        values = values.permute(0, 2, 1, 3).reshape(batch_size, sequence_length, d_model)
+        output = self.linear_layer(values)
+
+        return output
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob):
+        super(DecoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(d_model, num_heads)
+        self.layer_norm1 = LayerNormalization(parametres_shape=[d_model])
+        self.dropout1 = nn.Dropout(drop_prob)
+
+        self.encoder_decoder_attention = MultiHeadCrossAttention(d_model, num_heads)
+        self.layer_norm2 = LayerNormalization(parametres_shape=[d_model])
+        self.dropout2 = nn.Dropout(drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model, ffn_hidden, drop_prob)
+        self.layer_norm3 = LayerNormalization([d_model])
+        self.dropout3 = nn.Dropout(drop_prob)
+    
+    def forward(self, x, y, self_attention_mask, cross_attention_mask):
+        _y = y.clone()
+        y = self.self_attention(y, mask=self_attention_mask)
+        y = self.dropout1(y)
+        y = self.layer_norm1(y + _y)
+
+        _y = y.clone()
+        y = self.encoder_decoder_attention(x, y, mask=cross_attention_mask)
+        y = self.dropout2(y)
+        y = self.layer_norm2(y + _y)
+
+        _y = y.clone()
+        y = self.ffn(y)
+        y = self.dropout3(y)
+        y = self.layer_norm3(y + _y)
+
+        return y
+
+class SequentialDecoder(nn.Sequential):
+    def forward(self, *inputs):
+        x, y, self_attention_mask, cross_attention_mask = inputs
+
+        for module in self._modules.values():
+            y = module(x, y, self_attention_mask, cross_attention_mask)
+        
+        return y
+
+class Decoder(nn.Module):
+    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN):
+        super().__init__()
+        self.sentence_embedding = SentenceEmbedding(max_sequence_length, d_model, language_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.layers = SequentialDecoder(*[DecoderLayer(d_model, ffn_hidden, num_heads, drop_prob) for _ in range(num_layers)])
+
+    def forward(self, x, y, self_attention_mask, cross_attention_mask, START_TOKEN, END_TOKEN):
+        y = self.sentence_embedding(y, START_TOKEN, END_TOKEN)
+        y = self.layers(x, y, self_attention_mask, cross_attention_mask)
+
+        return y
+
+class Transformer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, kn_vocab_size, english_to_index, french_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN):
+        super().__init__()
+        self.encoder = Encoder(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, english_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.decoder = Decoder(d_model, ffn_hidden, num_heads, drop_prob, num_layers, max_sequence_length, french_to_index, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+        self.linear = nn.Linear(d_model, kn_vocab_size)
+        self.device = get_device()
+    
+    def forward(self, x, y, encoder_self_attention_mask=None, decoder_self_attention_mask=None, decoder_cross_attention_mask=None, enc_start_token=False, enc_end_token=False, dec_start_token=False, dec_end_token=False):
+        x = self.encoder(x, encoder_self_attention_mask, START_TOKEN=enc_start_token, END_TOKEN=enc_end_token)
+        output = self.decoder(x, y, decoder_self_attention_mask, decoder_cross_attention_mask, START_TOKEN=dec_start_token, END_TOKEN=dec_end_token)
+        output = self.linear(output)
+
+        return output
